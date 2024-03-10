d748f75cf706e520a43c001754aa13d19d7db4c8
Aramymo <84283648+Aramymo@users.noreply.github.com>
2024-03-10 15:56:31 +0300

    Вывод сообщения для удаления и подтверждение
    Добавлено подтверждение для удаления и вывод результата запроса

--- src/scripts/delete_node.js
+++ src/scripts/delete_node.js
@@ -3,23 +3,31 @@ $('#delete-node').submit(function(event){
         node_list: $('#select_delete_node').val(),
         form_type: $('#delete_form').val(),
     };
-    $.ajax({
-        url : "http://localhost:8888/app/Controller.php",
-        type: "POST",
-        dataType: "json",
-        data: formData,
-        encode: true,
-        success: function(data){
-            //отображение сообщения об успехе
-            getTreeData();
-            document.getElementById('delete_node_message').innerHTML = '';
-            document.getElementById("delete_node_message").innerHTML += "<div class='centered_text'>" + data["message"] + "</div>";
-        },
-        error: function(xhr, status, error){
-            //отображение сообщения об ошибке
-            console.error(xhr);
-        }
-    });
+    if(confirm("Удалить элемент и все его подкатегории?"))
+    {
+        $.ajax({
+            url : "http://localhost:8888/app/Controller.php",
+            type: "POST",
+            dataType: "json",
+            data: formData,
+            encode: true,
+            success: function(data){
+                //отображение сообщения об успехе
+                getTreeData();
+                document.getElementById('delete_node_message').innerHTML = '';
+                document.getElementById("delete_node_message").innerHTML += "<div class='centered_text'>" + data["message"] + "</div>";
+            },
+            error: function(xhr, status, error){
+                //отображение сообщения об ошибке
+                console.error(xhr);
+            }
+        });
+    }
+    else
+    {
+        document.getElementById('delete_node_message').innerHTML = '';
+        document.getElementById("delete_node_message").innerHTML += "<div class='centered_text'>Удаление отменено</div>";
+    }
     event.preventDefault();
 });
 
